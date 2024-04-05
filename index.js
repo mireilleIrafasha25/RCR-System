0ec1540569d817a65bf8d4bd3e746c0299c769a4
mireilleIrafasha25 <156303827+mireilleIrafasha25@users.noreply.github.com>
2024-04-05 11:11:08 +0200

    Swagger documentation

--- index.js
+++ index.js
@@ -2,44 +2,19 @@ import express from 'express'
 import mongoose from 'mongoose'
 import route from './routes/RCRroutes'
 import bodyParser from 'body-parser'
-import swaggerJSDoc from 'swagger-jsdoc'
+//import swaggerJSDoc from 'swagger-jsdoc'
+import swagger from './doc/swagger.json' assert {type:'json'};
 import  SwaggerUi from 'swagger-ui-express'
 const app=express()
 app.use(express.json())// middleware for  parsing json bodies
 app.use(bodyParser.json()); // Parse JSON-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
-app.use(route) //connecting RCR route
-mongoose.connect('mongodb+srv://mireilleirafasha:rHoAmGXZzzA8ktU2@cluster0.4bfpmyu.mongodb.net/RCRsystem') //
-const option={
-    definition:{
-        openai:"3.0.0",
-        info:
-        {
-     title:"RCR Api doc",
-     version:"0.1",
-     description:"This is simple RCR API application made by Express and documented by Swagger",
-     contact:
-     {
-        name:"Reached Children Rwanda",
-        url:"www.rcr.com",
-        email:"rcr250@gmail.com",
-     },
-        },
-        servers:
-        [
-            {
-            url: "http://localhost:6001/",
-            },
-        ],
-    },
-    apis:["./routes/*.jS"],
-};
-
-const spacs=swaggerJSDoc(option)
-app.use("/api_docs",
-SwaggerUi.serve,
-SwaggerUi.setup(spacs))
-
+app.use(route)
+ //connecting RCR route
+mongoose.connect('mongodb+srv://mireilleirafasha:WHwzvlIAgHlD3Rwx@cluster0.4bfpmyu.mongodb.net/RCRsystem') //
+//const spacs=swaggerJSDoc(option)
+//console.log(swagger)
+app.use("/api_docs",SwaggerUi.serve,SwaggerUi.setup(swagger))
 const ports=6001;
 app.listen(ports,()=>
 {
